4b1e0db332c3c7539f1c33d1136718e69e824591
ShimizuNaoki <sensuikan1973@gmail.com>
2020-11-01 04:20:22 +0900

    fix path bug

--- zsh/.eval.zsh
+++ zsh/.eval.zsh
@@ -1,5 +1,5 @@
 # 拡張子ごとの表示色
-eval "$(gdircolors ./extension/dircolors-solarized/dircolors.ansi-universal)"
+eval "$(gdircolors $HOME/dotfiles/extension/dircolors-solarized/dircolors.ansi-universal)"
 
 # pyenv
 eval "$(pyenv init -)"
--- zsh/.plugin.zsh
+++ zsh/.plugin.zsh
@@ -1,3 +1,3 @@
 # zsh-syntax-highlighting
-source $PWD/plugin/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/dotfiles/plugin/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets) # See: https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters.md#zsh-syntax-highlighting--highlighters
