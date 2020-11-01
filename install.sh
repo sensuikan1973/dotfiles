04fdc9a50e218c45df32b3f711633e4074e11fb5
naoki-a-shimizu <naoki.a.shimizu@dena.com>
2020-11-02 05:45:54 +0900

    ruby

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 brew install coreutils # gls を使いたい
+brew install rbenv ruby-build # ruby
 
 # == plugin ==
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/dotfiles/plugin/zsh-syntax-highlighting
