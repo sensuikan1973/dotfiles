21daa2573f4a820c03a9a5cc766e2e55b3e5e1ec
sensuikan1973 <sensuikan1973@gmail.com>
2022-02-15 02:48:12 +0900

    support Linux for dircolors-solarized

--- zsh/.plugin.zsh
+++ zsh/.plugin.zsh
@@ -5,7 +5,11 @@ source $zsh_plugins_dir/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets root) # See: https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters.md#zsh-syntax-highlighting--highlighters
 
 # dircolors-solarized
-eval "$(gdircolors $zsh_plugins_dir/dircolors-solarized/dircolors.ansi-universal)"
+if [ `uname` = "Darwin" ]; then
+  eval "$(gdircolors $zsh_plugins_dir/dircolors-solarized/dircolors.ansi-universal)"
+elif [ `uname` = "Linux" ]; then
+  eval "$(dircolors $zsh_plugins_dir/dircolors-solarized/dircolors.ansi-universal)"
+fi
 
 # `ctrl + R` で、履歴から選択できるようにする
 # NOTE: インストールは OS ごとそれぞれの方法ですること。これについては現状 macos しか対応してない。See: macos/setup.zsh
