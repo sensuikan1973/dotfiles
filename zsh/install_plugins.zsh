48a51b59bca6f987ddc048da2730997cfd88e0a9
sensuikan1973 <sensuikan1973@gmail.com>
2022-02-15 00:13:56 +0900

    refactoring

--- zsh/install_plugins.zsh
+++ zsh/install_plugins.zsh
@@ -2,20 +2,21 @@ set -euxo pipefail
 
 zsh_plugins_dir="$HOME/dotfiles/zsh/plugins"
 
-echo "install useful plugins"
+echo "install zsh plugins"
 
-echo "install/update https://github.com/zsh-users/zsh-syntax-highlighting.git"
-zsh_syntax_highlighting_repo_dir=$zsh_plugins_dir/zsh-syntax-highlighting
-if [ ! -d "$zsh_syntax_highlighting_repo_dir" ] ; then
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $zsh_syntax_highlighting_repo_dir
-else
-  cd $zsh_syntax_highlighting_repo_dir && git pull --prune
-fi
+function install_plugin() {
+  git_repository=$1
+  dest_dir="$zsh_plugins_dir/$git_repository"
+  if [ ! -d "$dest_dir" ] ; then
+    echo "clone $git_repository"
+    git clone $git_repository $dest_dir
+  else
+    echo "pull $git_repository"
+    cd $dest_dir && git pull --prune
+  fi
+}
 
-echo "install/update https://github.com/seebi/dircolors-solarized.git"
-dircolors_solarized_repo_dir=$zsh_plugins_dir/dircolors-solarized
-if [ ! -d "$dircolors_solarized_repo_dir" ] ; then
-  git clone https://github.com/seebi/dircolors-solarized.git $dircolors_solarized_repo_dir
-else
-  cd $dircolors_solarized_repo_dir && git pull --prune
-fi
+install_plugin "https://github.com/zsh-users/zsh-syntax-highlighting.git"
+install_plugin "https://github.com/seebi/dircolors-solarized.git"
+
+echo "zsh plugins has been installed"
