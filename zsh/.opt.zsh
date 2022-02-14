19215c7dcef9c8b020f8a73969ee02d504af3b8a
sensuikan1973 <sensuikan1973@gmail.com>
2022-02-15 07:42:57 +0900

    mild update doc

--- zsh/.opt.zsh
+++ zsh/.opt.zsh
@@ -13,40 +13,40 @@ setopt nolistbeep
 # notify when background job has finished.
 setopt no_tify
 
-# ディレクトリスタックに同じディレクトリを追加しない
+# directory stuck ignore duplicated directory.
 setopt pushd_ignore_dups
 
-# 補完候補が複数ある時に、一覧表示する
+# display completion lsit.
 setopt auto_list
 
-# auto_list の補完候補一覧で、ls -F のようにファイル種別をマーク表示する
+# display file type on completion.
 setopt list_types
 
-# 上書きリダイレクトを禁止
+# forbidden overwriting.
 setopt no_clobber
 
-# 余分な空白は詰める
+# pack redudant blanks.
 setopt hist_reduce_blanks
 
-# Ctrl+D では終了させない（exit や logout を使う）
+# forbidden logout by `Ctrl + D`.
 setopt ignore_eof
 
-# コマンドライン実行時にもコメントを使いたい
+# allow to comment on interactive shell.
 setopt interactivecomments
 
-# 辞書順ではなく数字順に並べる。
+# not alphabetical but numeric sort.
 setopt numeric_glob_sort
 
-# 同ホストで他のターミナルと履歴を共有
+# share history with other terminal on the same host.
 setopt share_history
 
-# 直前と同じコマンドラインはヒストリに追加しない
+# ignore duplicated command on history.
 setopt hist_ignore_dups
 
-# historyコマンドは履歴に残さない
+# ignore history command on history.
 setopt hist_no_store
 
-# 複数の zsh を同時に使う時など history ファイルに上書きせず追加
+# append history. not renew .zsh_history.
 setopt append_history
 
 # for peco, save many history.
