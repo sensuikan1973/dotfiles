8bb5648651a493d0e5bae17b521a6e6be1473e71
sensuikan1973 <sensuikan1973@gmail.com>
2022-02-15 00:04:16 +0900

    change zsh plugins dir

--- zsh/.plugin.zsh
+++ zsh/.plugin.zsh
@@ -1,6 +1,8 @@
+zsh_plugins_dir="$HOME/dotfiles/zsh/plugins"
+
 # zsh-syntax-highlighting
-source $HOME/dotfiles/zsh_plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets) # See: https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters.md#zsh-syntax-highlighting--highlighters
+source $zsh_plugins_dir/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets root) # See: https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters.md#zsh-syntax-highlighting--highlighters
 
 # dircolors-solarized
-eval "$(gdircolors $HOME/dotfiles/zsh_plugins/dircolors-solarized/dircolors.ansi-universal)"
+eval "$(gdircolors $zsh_plugins_dir/dircolors-solarized/dircolors.ansi-universal)"
--- zsh/install_plugins.zsh
+++ zsh/install_plugins.zsh
@@ -1,11 +1,11 @@
 set -euxo pipefail
 
-zsh_plugins_dir="zsh_plugins"
+zsh_plugins_dir="$HOME/dotfiles/zsh/plugins"
 
 echo "install useful plugins"
 
 echo "install/update https://github.com/zsh-users/zsh-syntax-highlighting.git"
-zsh_syntax_highlighting_repo_dir=$HOME/dotfiles/$zsh_plugins_dir/zsh-syntax-highlighting
+zsh_syntax_highlighting_repo_dir=$zsh_plugins_dir/zsh-syntax-highlighting
 if [ ! -d "$zsh_syntax_highlighting_repo_dir" ] ; then
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $zsh_syntax_highlighting_repo_dir
 else
@@ -13,7 +13,7 @@ else
 fi
 
 echo "install/update https://github.com/seebi/dircolors-solarized.git"
-dircolors_solarized_repo_dir=$HOME/dotfiles/$zsh_plugins_dir/dircolors-solarized
+dircolors_solarized_repo_dir=$zsh_plugins_dir/dircolors-solarized
 if [ ! -d "$dircolors_solarized_repo_dir" ] ; then
   git clone https://github.com/seebi/dircolors-solarized.git $dircolors_solarized_repo_dir
 else
