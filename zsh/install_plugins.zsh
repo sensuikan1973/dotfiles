1ae52acd61541ab90dcf67579aafd39819ce6459
sensuikan1973 <sensuikan1973@gmail.com>
2022-02-15 00:05:04 +0900

    fix pull command

--- zsh/install_plugins.zsh
+++ zsh/install_plugins.zsh
@@ -9,7 +9,7 @@ zsh_syntax_highlighting_repo_dir=$zsh_plugins_dir/zsh-syntax-highlighting
 if [ ! -d "$zsh_syntax_highlighting_repo_dir" ] ; then
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $zsh_syntax_highlighting_repo_dir
 else
-  cd $zsh_syntax_highlighting_repo_dir && git remote update --prune && git pull
+  cd $zsh_syntax_highlighting_repo_dir && git pull --prune
 fi
 
 echo "install/update https://github.com/seebi/dircolors-solarized.git"
@@ -17,5 +17,5 @@ dircolors_solarized_repo_dir=$zsh_plugins_dir/dircolors-solarized
 if [ ! -d "$dircolors_solarized_repo_dir" ] ; then
   git clone https://github.com/seebi/dircolors-solarized.git $dircolors_solarized_repo_dir
 else
-  cd $dircolors_solarized_repo_dir && git remote update --prune && git pull
+  cd $dircolors_solarized_repo_dir && git pull --prune
 fi
