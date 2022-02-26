a2949a4c6c34f72cd5bb3acea9098a5e609f6756
sensuikan1973 <sensuikan1973@gmail.com>
2022-02-26 18:39:07 +0900

    brew install iterm2 --cask

--- setup.zsh
+++ setup.zsh
@@ -33,6 +33,7 @@ brew install rbenv ruby-build # want to use multiple version ruby
 brew install pyenv # want to use multiple version python
 brew install zsh # want to use latest zsh
 brew install peco # select command in history
+brew install iterm2 --cask
 
 # set zsh to login shell
 sudo ln -fs $resource_dir/etc/shells /etc/shells
