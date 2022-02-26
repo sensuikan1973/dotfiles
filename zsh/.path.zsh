5cad726c0d2843bf1657dd169210fc4dc47feded
sensuikan1973 <sensuikan1973@gmail.com>
2022-02-26 21:01:58 +0900

    add PATH

--- zsh/.path.zsh
+++ zsh/.path.zsh
@@ -4,6 +4,9 @@ export PATH=$PATH:/usr/local/share/git-core/contrib/diff-highlight
 # elsticsearch@6
 export PATH="/usr/local/opt/elasticsearch@6/bin:$PATH"
 
+# See: https://formulae.brew.sh/formula/nodebrew
+export NODEBREW_ROOT=$(brew --prefix)/var/nodebrew
+
 # Android
 export PATH=$PATH:~/Library/Android/sdk/platform-tools
 
