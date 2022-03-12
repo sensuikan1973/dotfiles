ffa12f15554a881defbf84ec80204cc30e2e6f7f
sensuikan1973 <sensuikan1973@gmail.com>
2022-03-13 05:10:15 +0900

    add screencapture location

--- macos/defaults/screencapture.zsh
+++ macos/defaults/screencapture.zsh
@@ -1,2 +1,5 @@
 # Include date and time in screenshot filenames.
 defaults write com.apple.screencapture include-date -bool true
+
+# set default screenshot location.
+defaults write com.apple.screencapture location -string "~/Desktop"
