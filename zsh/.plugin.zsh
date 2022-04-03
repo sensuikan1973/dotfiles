5ca865406fa2108f6a0ccfb494edfbe238db934a
sensuikan1973 <sensuikan1973@gmail.com>
2022-04-03 17:54:12 +0900

    disable SC2034

--- zsh/.plugin.zsh
+++ zsh/.plugin.zsh
@@ -1,11 +1,11 @@
-# shellcheck disable=SC1091
+# shellcheck disable=SC1091,SC2034
 
 zsh_plugins_dir="$HOME/dotfiles/zsh/plugins"
 
 # zsh-syntax-highlighting
 # See: https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters.md#zsh-syntax-highlighting--highlighters
 source "$zsh_plugins_dir/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-export ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets root)
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets root)
 
 # dircolors-solarized
 eval "$(gdircolors "$zsh_plugins_dir/dircolors-solarized/dircolors.ansi-universal")"
