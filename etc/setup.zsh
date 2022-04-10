0229aab9b44ada3daa3da9959b90ca8d63ce024b
sensuikan1973 <sensuikan1973@gmail.com>
2022-04-10 23:07:01 +0900

    fix SC2154

--- etc/setup.zsh
+++ etc/setup.zsh
@@ -3,7 +3,7 @@
 if [ "$(diff "$HOME/dotfiles/etc/shells" /etc/shells)" != "" ]; then
   echo "changing /etc/shells"
   mkdir "$HOME/dotfiles/.backup/etc"
-  cat /etc/shells > "$HOME/dotfiles/.backup/etc/shells.${date -I seconds}"
+  cat /etc/shells > "$HOME/dotfiles/.backup/etc/shells.$(date -I seconds)"
   sudo ln -fs "$HOME/dotfiles/etc/shells" /etc/shells
 fi
 
