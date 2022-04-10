78204f2ecbe66fb0ac687e6d5eab51e9d98dbe15
done <23427957+sensuikan1973@users.noreply.github.com>
2022-04-10 16:37:45 +0900

    disable to install Rdoc (#81)
    * disable to install Rdoc
    
    * fix SC2034
    
    * fix workflow trigger

--- setup.zsh
+++ setup.zsh
@@ -45,6 +45,13 @@ if [ "${SHELL:-undefined}" != "$(brew --prefix)/bin/zsh" ]; then
 fi
 
 # ruby gem
+#
+# save time and space. I don't need Rdoc.
+# https://github.com/rbenv/ruby-build/issues/156
+# https://github.com/rbenv/ruby-build#custom-build-configuration
+# https://github.com/ruby/ruby/commit/32e00d382f4ef204ac21868ded101597e36d230d
+export RUBY_CONFIGURE_OPTS="--disable-install-doc"
+
 ln -fs "$resource_dir/ruby/.gemrc" "$HOME/.gemrc"
 for rb_version in $(rbenv versions --bare); do
   echo "installing ruby $rb_version"
