d8329f75a648dda5dffc05fa1244fe929d5fe1b1
sensuikan1973 <sensuikan1973@gmail.com>
2022-04-10 23:06:13 +0900

    add comment

--- etc/setup.zsh
+++ etc/setup.zsh
@@ -7,6 +7,7 @@ if [ "$(diff "$HOME/dotfiles/etc/shells" /etc/shells)" != "" ]; then
   sudo ln -fs "$HOME/dotfiles/etc/shells" /etc/shells
 fi
 
+# set zsh to login shell
 if [ "${SHELL:-undefined}" != "$(brew --prefix)/bin/zsh" ]; then
   echo "changing login shell"
   sudo chsh -s "$(brew --prefix)/bin/zsh" "$(whoami)"
