7853d262bcbe426304ffbf6ed5d015b8eace04c5
done <23427957+sensuikan1973@users.noreply.github.com>
2022-05-29 07:34:32 +0900

    use not nodebrew but nodenv (#92)
    * use not nodebrew but nodenv
    
    * remove workaround for macos on workflow

--- zsh/.path.zsh
+++ zsh/.path.zsh
@@ -7,11 +7,6 @@ export HOMEBREW_BUNDLE_FILE="$HOME/dotfiles/Brewfile"
 # use diff-highlight with git
 export PATH=$PATH:/usr/local/share/git-core/contrib/diff-highlight
 
-# See: https://formulae.brew.sh/formula/nodebrew
-nodebrew_root="$(brew --prefix)/var/nodebrew"
-export NODEBREW_ROOT=$nodebrew_root
-export PATH=$PATH:$NODEBREW_ROOT/current/bin
-
 # Android
 export PATH=$PATH:~/Library/Android/sdk/platform-tools
 
