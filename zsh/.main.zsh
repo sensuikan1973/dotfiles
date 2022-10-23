0d2a5a451cdd4b0d88c110bd8776eb2941a97223
sensuikan1973 <sensuikan1973@gmail.com>
2022-10-24 02:34:10 +0900

    compinit -i

--- zsh/.main.zsh
+++ zsh/.main.zsh
@@ -7,7 +7,7 @@ ZSH_SETTINGS_PATH="$HOME/dotfiles/zsh"
 # completion
 # contain Git completion: https://git-scm.com/book/ja/v2/Appendix-A%3A-%E3%81%9D%E3%81%AE%E4%BB%96%E3%81%AE%E7%92%B0%E5%A2%83%E3%81%A7%E3%81%AEGit-Zsh%E3%81%A7Git%E3%82%92%E4%BD%BF%E3%81%86#roh_my_zsh_git
 # See: 「man zshcompsys」
-autoload -Uz compinit && compinit -u
+autoload -Uz compinit && compinit -i
 
 # PATH
 source "$ZSH_SETTINGS_PATH/.path.zsh"
