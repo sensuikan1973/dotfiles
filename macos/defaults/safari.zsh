7e28c2e0ab14237ecda5ecd5891d8f971562050d
sensuikan1973 <sensuikan1973@gmail.com>
2022-10-24 01:20:38 +0900

    defaults write com.apple.Safari

--- macos/defaults/safari.zsh
+++ macos/defaults/safari.zsh
@@ -1,3 +1,6 @@
 # not send search query to Apple
 defaults write com.apple.Safari SuppressSearchSuggestions -bool true
 defaults write com.apple.Safari UniversalSearchEnabled -bool false
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
