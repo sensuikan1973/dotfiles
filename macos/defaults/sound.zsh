bd8a39d89ad86510649bbab80c97e1c1b9add0ab
sensuikan1973 <sensuikan1973@gmail.com>
2022-11-25 01:46:01 +0900

    fix

--- macos/defaults/sound.zsh
+++ macos/defaults/sound.zsh
@@ -1,5 +1,5 @@
 domain="com.apple.sound"
 
-defaults write "$domain.beep" feedback -bool false
-defaults write "$domain.beep" volume -int 0
-defaults write "$domain.uiaudio" enabled -bool false
+defaults write "$domain.beep.feedback" -bool false
+defaults write "$domain.beep.volume" -int 0
+defaults write "$domain.uiaudio.enabled" -bool false
