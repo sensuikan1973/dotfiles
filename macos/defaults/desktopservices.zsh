ef1d94124adf72b3afd4fc7539d6cbf69e910944
sensuikan1973 <sensuikan1973@gmail.com>
2023-04-03 08:48:42 +0900

    add comment

--- macos/defaults/desktopservices.zsh
+++ macos/defaults/desktopservices.zsh
@@ -1,5 +1,6 @@
 domain="com.apple.desktopservices"
 
 # not create .DS_Store in USB, NetworkStores.
+# See: https://support.apple.com/ja-jp/HT208209
 defaults write "$domain" DSDontWriteNetworkStores -bool true
 defaults write "$domain" DSDontWriteUSBStores -bool true
