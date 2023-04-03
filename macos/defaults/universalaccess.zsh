07b50cb02dd474f71bb277f416dec47c916670ce
done <23427957+sensuikan1973@users.noreply.github.com>
2023-04-03 21:35:53 +0900

    Update universalaccess.zsh

--- macos/defaults/universalaccess.zsh
+++ macos/defaults/universalaccess.zsh
@@ -1,3 +1,5 @@
 domain="com.apple.universalaccess"
 
-defaults write "$domain" showWindowTitlebarIcons -bool true
+# Maybe, From macos13 this domain is invalid.
+# Error message "defaults[20678:68564] Could not write domain com.apple.universalaccess; exiting"
+# defaults write "$domain" showWindowTitlebarIcons -bool true
