94b11b23b3bb3d9d1a5cfa72f619b36b3062b1ff
done <23427957+sensuikan1973@users.noreply.github.com>
2023-04-03 21:23:18 +0900

    Update setup.zsh

--- macos/setup.zsh
+++ macos/setup.zsh
@@ -3,12 +3,14 @@
 set -euxo pipefail
 
 # defaults
-# See: https://github.com/koalaman/shellcheck/wiki/SC2044
-while IFS= read -r -d '' file
+# See: https://github.com/koalaman/shellcheck/wiki/SC2044#for-posix
+find "$HOME/dotfiles/macos/defaults" ! -name "$(printf "*\n*")" -type f -name "*.zsh" > tmp
+while IFS= read -r file
 do
   echo "run $file"
   source "$file"
-done < <(find "$HOME/dotfiles/macos/defaults" -type f -name "*.zsh")
+done < tmp
+rm tmp
 # NOTE: Night Shift はスクリプト書かないと無理なので、ひとまずは手動で済ませて
 # REF: https://gist.github.com/thomasfinch/14bd3181799734c872d2ad3b207cc01c
 
