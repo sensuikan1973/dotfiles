a52bd7843cabd8c522336d65db7114f44df6081c
done <23427957+sensuikan1973@users.noreply.github.com>
2023-04-03 22:35:47 +0900

    Update .path.zsh

--- zsh/.path.zsh
+++ zsh/.path.zsh
@@ -3,10 +3,11 @@ export PATH=$PATH:$HOME/dotfiles/zsh/bin
 
 # brew
 export HOMEBREW_BUNDLE_FILE="$HOME/dotfiles/Brewfile"
-export PATH="$(brew --prefix)/sbin:$PATH"
+brew_prefix="$(brew --prefix)"
+export PATH="$brew_prefix/sbin:$PATH"
 
 # use diff-highlight with git
-export PATH=$PATH:$(brew --prefix)/share/git-core/contrib/diff-highlight
+export PATH=$PATH:$brew_prefix/share/git-core/contrib/diff-highlight
 
 # Android
 export PATH=$PATH:~/Library/Android/sdk/platform-tools
@@ -17,5 +18,3 @@ export PATH=$PATH:$GOPATH/bin
 
 # Dart Global Pub
 export PATH="$PATH":"$HOME/.pub-cache/bin"
-
-a
