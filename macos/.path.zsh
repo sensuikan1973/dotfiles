cecedc314e6698f80723f5a03868241478e9e888
done <23427957+sensuikan1973@users.noreply.github.com>
2023-04-11 20:51:00 +0900

    Update .path.zsh

--- macos/.path.zsh
+++ macos/.path.zsh
@@ -2,8 +2,8 @@
 # https://docs.brew.sh/FAQ#what-does-keg-only-mean
 
 # my functions
-export PATH=$PATH:$HOME/dotfiles/macos/bin
+export PATH="$PATH:$HOME/dotfiles/macos/bin"
 
 # brew
-export PATH=/opt/homebrew/bin:$PATH
-export PATH=/opt/homebrew/opt/**/bin:$PATH
+export PATH="/opt/homebrew/bin:$PATH"
+export PATH="/opt/homebrew/opt/**/bin:$PATH"
