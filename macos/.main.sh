9e8d7b2c6fd4582c6877df742926ac5dfe7693c2
sensuikan1973 <sensuikan1973@gmail.com>
2023-04-13 04:20:03 +0900

    fix

--- macos/.main.sh
+++ macos/.main.sh
@@ -5,4 +5,5 @@ source "$HOME/dotfiles/macos/.chflags.sh"
 source "$HOME/dotfiles/macos/.plugin.sh"
 
 # See: https://formulae.brew.sh/cask/google-cloud-sdk
-source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
+source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
