aa5fce84a6cf697ce9d7bff25967d52794197346
sensuikan1973 <sensuikan1973@gmail.com>
2023-04-13 11:21:31 +0900

    mild refactoring

--- nodejs/setup.sh
+++ nodejs/setup.sh
@@ -1,3 +1,4 @@
-# https://github.com/nodenv/nodenv#using-package-managers
 eval "$(nodenv init -)"
+
+# https://github.com/nodenv/nodenv#using-package-managers
 curl -fsSL https://github.com/nodenv/nodenv-installer/raw/master/bin/nodenv-doctor | bash
