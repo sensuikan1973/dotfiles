e3b78391df1954f6470397577ca516672a208386
sensuikan1973 <sensuikan1973@gmail.com>
2023-05-29 09:05:57 +0900

    fix source path

--- zsh/.plugin.zsh
+++ zsh/.plugin.zsh
@@ -1,10 +1,9 @@
 # shellcheck disable=SC1091,SC2034,SC2153
 
-zsh_plugins_dir="$HOME/dotfiles/zsh/plugins"
-
 # zsh-syntax-highlighting
+# https://formulae.brew.sh/formula/zsh-syntax-highlighting
 # See: https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters.md#zsh-syntax-highlighting--highlighters
-source "$zsh_plugins_dir/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+source "$(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets root)
 
 # select command in history with `ctrl + R`.
