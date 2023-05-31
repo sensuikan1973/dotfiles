90270c861d0725e4efcd24946a832acd6e9a3060
sensuikan1973 <sensuikan1973@gmail.com>
2023-06-01 00:53:05 +0900

    npm install -g npm@latest

--- nodejs/setup.sh
+++ nodejs/setup.sh
@@ -2,3 +2,7 @@ eval "$(nodenv init -)"
 
 # https://github.com/nodenv/nodenv#using-package-managers
 curl -fsSL https://github.com/nodenv/nodenv-installer/raw/master/bin/nodenv-doctor | bash
+
+# https://github.com/nodenv/nodenv#understanding-shims
+# https://docs.npmjs.com/try-the-latest-stable-version-of-npm
+npm install -g npm@latest
