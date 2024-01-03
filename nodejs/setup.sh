6511ff2aa76e8431305c49e087a48e18ae9f55d7
sensuikan1973 <23427957+sensuikan1973@users.noreply.github.com>
2024-01-03 10:23:52 +0900

    Update setup.sh

--- nodejs/setup.sh
+++ nodejs/setup.sh
@@ -2,3 +2,7 @@ eval "$(nodenv init -)"
 
 # https://github.com/nodenv/nodenv#using-package-managers
 curl -fsSL https://github.com/nodenv/nodenv-installer/raw/master/bin/nodenv-doctor | bash
+
+# https://yarnpkg.com/getting-started/install
+# for https://github.com/sensuikan1973/pedax/tree/main/website
+corepack enable
