11ac597d4648d4ba70af8215a47c536e796a70b5
sensuikan1973 <23427957+sensuikan1973@users.noreply.github.com>
2024-01-23 08:36:34 +0900

    Update setup.sh (#155)

--- nodejs/setup.sh
+++ nodejs/setup.sh
@@ -5,8 +5,10 @@ curl -fsSL https://github.com/nodenv/nodenv-installer/raw/master/bin/nodenv-doct
 
 # https://yarnpkg.com/getting-started/install
 # for https://github.com/sensuikan1973/pedax/tree/main/website
-if which corepack ; then
-  if node --version; then
-    corepack enable
-  fi
-fi
+for node_version in $(nodenv versions --bare); do
+  NODENV_VERSION="$node_version"
+  echo "$NODENV_VERSION"
+  nodenv shell "$node_version"
+  nodenv versions
+  corepack enable
+done
