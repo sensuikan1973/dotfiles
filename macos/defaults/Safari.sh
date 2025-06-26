adf87a8e5e2661f9a2c02e2626e3823901c287c4
sensuikan1973 <sensuikan1973@gmail.com>
2025-06-26 18:08:37 +0900

    del com.apple.Safari

--- macos/defaults/Safari.sh
+++
@@ -1,8 +0,0 @@
-domain="com.apple.Safari"
-
-# not send search query to Apple
-defaults write "$domain" SuppressSearchSuggestions -bool true
-defaults write "$domain" UniversalSearchEnabled -bool false
-defaults write "$domain" IncludeDevelopMenu -bool true
-defaults write "$domain" IncludeInternalDebugMenu -bool true
-defaults write "$domain" WebKitDeveloperExtrasEnabledPreferenceKey -bool true
