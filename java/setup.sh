a9e904d803ee37eef408980baf20d7ea61051dc2
sensuikan1973 <sensuikan1973@gmail.com>
2025-12-29 12:50:35 +0900

    SC2086

--- java/setup.sh
+++ java/setup.sh
@@ -1,3 +1,3 @@
 # https://zenn.dev/hayato94087/articles/c0345e6c2c53e7
 
-sudo ln -sfn $HOMEBREW_PREFIX/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+sudo ln -sfn "$HOMEBREW_PREFIX/opt/openjdk/libexec/openjdk.jdk" "/Library/Java/JavaVirtualMachines/openjdk.jdk"
